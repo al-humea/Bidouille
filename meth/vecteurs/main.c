8e9a02b0224720db5e483381cc8d5c7ad4b23ac4
al-humea <alex.lhumeau@outlook.fr>
2025-09-04 22:47:06 +0200

    having fun

--- meth/vecteurs/main.c
+++ meth/vecteurs/main.c
@@ -1,8 +1,38 @@
-#include <stdio.h>
 #include "vectors.h"
 
 int main(void) {
-    printf("Expérimentations vectorielles\n");
-    
+    srand(time(NULL));
+    printf("Expérimentations vectorielles:\n");
+    printf("Initialisation de deux vecteurs 2D puis 3D aléatoire...\n");
+    vec2 *a, *b;
+    vec3 *c, *d;
+    a = malloc(sizeof(vec2));
+    a->x = rand() % 200 - 100;
+    a->y = rand() % 200 - 100;
+    printf("v2a={%.3f, %.3f}\n", a->x, a->y);
+    b = malloc(sizeof(vec2));
+    b->x = rand() % 200 - 100;
+    b->y = rand() % 200 - 100;
+    printf("v2b={%.3f, %.3f}\n", b->x, b->y);
+    c = malloc(sizeof(vec3));
+    c->x = rand() % 200 - 100;
+    c->y = rand() % 200 - 100;
+    c->z = rand() % 200 - 100;
+    printf("v3c={%.3f, %.3f}\n", c->x, c->y, c->z);
+    d = malloc(sizeof(vec3));
+    d->x = rand() % 200 - 100;
+    d->y = rand() % 200 - 100;
+    d->z = rand() % 200 - 100;
+    printf("v3d={%.3f, %.3f}\n", d->x, d->y, d->z);
+
+
+    b = malloc(sizeof(vec2));
+    c = malloc(sizeof(vec3));
+    d = malloc(sizeof(vec3));
+
+    free(a);
+    free(b);
+    free(c);
+    free(d);
     return 0;
 }
--- meth/vecteurs/vectors.h
+++ meth/vecteurs/vectors.h
@@ -1,9 +1,24 @@
 #ifndef VECTORS_H
 # define VECTORS_H
-
+# include <stdio.h>
 # include <stdlib.h>
+# include <time.h>
 //vectors definition
-typedef struct vec2{int a; int b;} vec2;
-typedef struct vec3{int a; int b; int c;} vec3;
+typedef struct vec2{float x; float y;} vec2;
+typedef struct vec3{float x; float y; float z;} vec3;
+
+vec2 v2add(vec2 a, vec2 b);
+vec3 v3add(vec3 a, vec3 b);
+vec2 v2sub(vec2 a, vec2 b);
+vec3 v3sub(vec3 a, vec3 b);
+vec2 v2scal(float scal, vec2 a);
+vec3 v3scal(float scal, vec3 a);
+float v2norm(vec2 a);
+float v3norm(vec3 a);
+vec2 v2u(vec2 a);
+vec3 v3u(vec3 a);
+vec2 v2dist(vec2 a, vec2 b);
+vec3 v3dist(vec3 a, vec3 b);
+
 
 #endif 
